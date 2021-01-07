963a2528d3c34571175a24aa720063143fb11820
Parkeymon <68636315+Parkeymon@users.noreply.github.com>
2021-01-07 10:57:02 -0800

    kind of works now

--- commands/verify.js
+++ commands/verify.js
@@ -4,11 +4,16 @@ module.exports = {
     async execute(message, args, Discord, client){
 
         //Set guild ID and verification channel ID
-        const verifychannel = client.channels.cache.get('796090442973970432')
+        const verifychannel = client.channels.cache.get('796090442973970432');
         const guild = client.guilds.fetch('576618331105394720')
-        .then(guild => console.log(guild.name))
-        .catch(console.error);
-        const member = message.author.id;
+        .then(async guild => {
+            console.log(guild.name)
+            console.log(guild.roles.cache.get('667959578948010004'))
+            console.log(message.author.id)
+      
+            let memberRole = guild.roles.cache.get('667959578948010004');    
+            const member = (message.author.id);
+            let memberTarget = guild.members.cache.get(member.id);
 
         //Asks user to put name
         const nameEmbed = new Discord.MessageEmbed()
@@ -17,51 +22,44 @@ module.exports = {
         .setDescription('Reply within 15 seconds or you must verify again. Please wait after sending.')
 
         message.channel.send(nameEmbed)
-
-        //Waits for response from user and sends verification message.
-/*        const nameCollector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 15000 });
-
-        nameCollector.on('collect', (message, col) => {
-            console.log(`INPUTTED NAME: ${message.content}`)
-            const verificationQueueEmbed = new Discord.MessageEmbed()
-            .setTitle('Pending Verification')
-            .addFields(
-                {name: 'Username of pending user:', value: `\`${message.author.username}\``},
-                {name: 'Inputted Name:', value: `${message.content}`}
-            )
-            verifychannel.send(verificationQueueEmbed)
-            .then(function(message) {
-                message.react('✅')
-                message.react('⛔')
-            }) 
-            
-            
-            message.channel.send('Your request for verification has been sent.')
-            
-        });
-
-        //Name collector timeout.
-        nameCollector.on('end', colected => {
-            message.channel.send(':no_entry: You waited too long! Try again. :no_entry:')
-        });*/
-
-
         
         try{
             let filter = m => m.author.id === message.author.id;
-            let msg = await message.channel.awaitMessages(filter, { maxMatcher: 1, time: '15000', error: ['time']})
+            let msg = await message.channel.awaitMessages(filter, { maxMatcher: 1, time: '5000', error: ['time']})
             const verificationQueueEmbed = new Discord.MessageEmbed()
             .setTitle('Pending Verification')
             .addFields(
-                {name: 'Username of pending user:', value: `\`${message.author.username}\``},
+                {name: 'Username of pending user:', value: `<@${message.author.id}>`},
                 {name: 'Inputted Name:', value: `${msg.first().content}`},
             )
             .setFooter('✅Approve | ⛔Deny')
             message.channel.send('Your request for verification has been sent. Please wait for approval.')
             verifychannel.send(verificationQueueEmbed)
-            .then(function(message) {
+            .then(async function(message) {
                 message.react('✅')
                 message.react('⛔')
+
+                member.setNickname(`${message.author.username} + (${msg.first().content})`)
+                .then(console.log('Nickname Changed.'));
+
+                await message.react('✅')
+                const filter = (reaction, user) => {
+                    return('test')
+                };
+                const reactCollector = new Discord.ReactionCollector(message, filter)
+
+                reactCollector.on('collect', (reaction, ReactionCollector) => {
+
+                    if(reactCollector.collected.first().emoji.name == '⛔') {
+                        message.channel.send('⛔ | User denied.')
+                        reactCollector.stop()
+                    } else {
+                        console.log('SOMTHING DID SOMETHING')
+                        message.channel.send('✅ | User Verified!')
+                        message.guild.roles.add(memberRole)
+                        reactCollector.stop()
+                    }
+                })
             }) 
             
         }
@@ -69,6 +67,7 @@ module.exports = {
             message.channel.send('You waited too long! Retry.')
         }
 
-        
+    })
+    
     }
 }
