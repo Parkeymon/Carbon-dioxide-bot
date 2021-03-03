67ee0d7b0203ca0a3267e360ae8503518707741a
Parkeymon <68636315+Parkeymon@users.noreply.github.com>
2021-03-02 18:25:43 -0800

    KILL THEM ALL

---
+++ commands/murder.js
@@ -0,0 +1,32 @@
+module.exports = {
+    name: 'murder',
+    description: 'Lets you murder your friends',
+    aliases: [
+        'kill',
+        'bringdeathupon'
+    ],
+    execute(message, args, Discord){
+
+        //Create death methods
+        var deathMethod = [
+            `They were strangled to death with your thicc anime thighs.`,
+            `They looked at your ugly face and suddenly suffered a heart attack.`,
+            `You made them realize they had to do their math homework at 11:58PM.`,
+            `You told them green day is a good band and they screamed so loud they exploded.`,
+            `You told them that they were your little pogchamp a they just fucking died, no clue why.`
+        ]
+        //Randomize
+        var deaths = Math.floor(Math.random() * deathMethod.length);
+
+        //Make le embed
+        const deathEmbed = new Discord.MessageEmbed()
+        .setTitle(`You killed ${message.mentions.members.first().nickname}!`)
+        .setDescription(`${deathMethod[deaths]}`)
+
+        if(!message.mentions.members.first()){
+            message.channel.send('You didnt mention anyone stoopid...');
+        } else {
+            message.channel.send(deathEmbed);
+        }
+    }
+}
